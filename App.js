c1330206df5398feda141032d710553dd4cfebad
brauliojaureguy12 <brauliojaureguy12@gmail.com>
2023-04-22 16:56:47 -0300

    dsf

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>fdsfds!!!!</Text>
+      <Text>fdsf!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
